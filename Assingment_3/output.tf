7c973898cd062b4a00b5ac5212746cd0c1b821a6
mayursabade <mayur_sabade@persistent.com>
2023-04-24 21:42:55 +0530

    output for domain name

--- Assingment_3/output.tf
+++ Assingment_3/output.tf
@@ -2,6 +2,6 @@ output "rest_api_url" {
   value = module.api_gateway.rest_api_url
 }
 
-# output "cdn_domain_name" {
-#   value = module.Cloudfront.cdn_domain_name
-# }
+output "cdn_domain_name" {
+  value = module.Cloudfront.cloudfront_domain_name
+}
