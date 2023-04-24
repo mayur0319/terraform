07a922377a04e431df954427dca6bd45b808ab29
mayursabade <mayur_sabade@persistent.com>
2023-04-24 21:41:58 +0530

    update target origin id

--- Assingment_3/cloudfront/cloudfront.tf
+++ Assingment_3/cloudfront/cloudfront.tf
@@ -8,14 +8,6 @@ resource "aws_cloudfront_distribution" "cf_dist" {
   enabled = true
 
   origin {
-    domain_name = var.bucket_domain_name
-    origin_id   = var.bucket_domain_id
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
-    }
-  }
-
-  origin {
     domain_name = "${var.aws_rest_api_id}.execute-api.${var.region}.amazonaws.com"
     origin_path = "/${var.rest_api_stage_name}"
     origin_id   = "Custome-${var.aws_rest_api_id}.execute-api.${var.region}.amazonaws.com"
@@ -30,7 +22,7 @@ resource "aws_cloudfront_distribution" "cf_dist" {
   default_cache_behavior {
     allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
     cached_methods         = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id       = var.bucket_domain_id
+    target_origin_id       = "Custome-${var.aws_rest_api_id}.execute-api.${var.region}.amazonaws.com"
     viewer_protocol_policy = "redirect-to-https"
     forwarded_values {
       headers      = []
