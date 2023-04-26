9c98200cd960b3c9ed8035c88f98b90db16e7680
mayursabade <mayur_sabade@persistent.com>
2023-04-26 18:35:31 +0530

    remove unused code part

--- Assignment_4_codepipeline/main.tf
+++ Assignment_4_codepipeline/main.tf
@@ -1,28 +1,3 @@
 provider "aws" {
   region = var.region
 }
-
-resource "aws_codebuild_project" "tfplan" {
-  name         = "tf-cicd-plan2"
-  description  = "Plan stage for terraform"
-  service_role = aws_iam_role.tf-codebuild-role.arn
-
-  artifacts {
-    type = "CODEPIPELINE"
-  }
-
-  environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "hashicorp/terraform:0.14.3"
-    type                        = "LINUX_CONTAINER"
-    image_pull_credentials_type = "SERVICE_ROLE"
-    registry_credential {
-      credential          = var.dockerhub_credentials
-      credential_provider = "SECRETS_MANAGER"
-    }
-  }
-  source {
-    type      = "CODEPIPELINE"
-    buildspec = file("buildspec/plan-buildspec.yml")
-  }
-}
