1f7bda28813b62d8d2fbc2fd03ac4084832d2033
mayursabade <mayur_sabade@persistent.com>
2023-04-26 20:51:32 +0530

    add codebuild for plan

--- Assignment_4_codepipeline/codepipeline.tf
+++ Assignment_4_codepipeline/codepipeline.tf
@@ -1,5 +1,5 @@
 resource "aws_codebuild_project" "tf-plan" {
-  name          = "tf-cicd-plan2"
+  name          = "cicd-plan"
   description   = "Plan stage for terraform"
   service_role  = aws_iam_role.tf-codebuild-role.arn
 
@@ -20,5 +20,30 @@ resource "aws_codebuild_project" "tf-plan" {
  source {
      type   = "CODEPIPELINE"
      buildspec = file("buildspec/plan-buildspec.yml")
+ }
+}
+
+resource "aws_codebuild_project" "tf-apply" {
+  name          = "cicd-apply"
+  description   = "Apply stage for terraform"
+  service_role  = aws_iam_role.tf-codebuild-role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "hashicorp/terraform:0.14.3"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "SERVICE_ROLE"
+    registry_credential{
+        credential = var.dockerhub_credentials
+        credential_provider = "SECRETS_MANAGER"
+    }
+ }
+ source {
+     type   = "CODEPIPELINE"
+     buildspec = file("buildspec/apply-buildspec.yml")
  }
 }
