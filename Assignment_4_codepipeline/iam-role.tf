0bb70e6ad4f531941c27bdd9bd3a9ae50f31edb5
mayursabade <mayur_sabade@persistent.com>
2023-04-26 18:57:07 +0530

    added iam role

---
+++ Assignment_4_codepipeline/iam-role.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_role" "codepipeline-iam-role" {
+  name = "codepipeline-iam-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
